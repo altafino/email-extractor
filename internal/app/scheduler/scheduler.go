cee0e9165b15b8bc0549bbf2e045fc3eb4852ae2
Stefan <st.wuthrich@gmail.com>
2025-02-25 13:24:55 -0300

    remove old scheduler.go

--- internal/app/scheduler/scheduler.go
+++
@@ -1,8 +0,0 @@
-// NewScheduler creates a new scheduler instance
-func NewScheduler(logger *slog.Logger, configs []*types.Config, tracker tracking.Tracker) *Scheduler {
-	return &Scheduler{
-		configs: configs,
-		logger:  logger,
-		tracker: tracker,
-	}
-} 
